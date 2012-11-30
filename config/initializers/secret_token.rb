24a1cd9fd08c7383a56d01f85ded2ccf98f42d01
whitperson <whitperson@gmail.com>
2012-11-30 10:53:04 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ghpractice::Application.config.secret_token = '7c5af77a2f605e1a2b1e8d57ad729ae6f7c02dfde358d0b91065b1756c6f3c13ebf7f9642552f0cbb00814fa173820ce7a52bc47d9458547d45987c68025a47b'
